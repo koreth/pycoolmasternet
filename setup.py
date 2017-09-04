7e03cb61d9dd047fa88f732fd583a5ff7ff715af
Steven Grimm <koreth@gmail.com>
2017-09-04 15:04:58 -0700

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name = 'pycoolmasternet',
-    version = '0.0.1',
+    version = '0.0.2',
     license = 'MIT',
     description = 'Lightweight Python API for CoolMasterNet HVAC bridges',
     long_description = long_description,
